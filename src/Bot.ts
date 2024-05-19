54c976d3ecf717a74bce0ade56ad3fa15ce1ea24
eliza <sech1p@disroot.org>
2024-05-19 19:41:10 +0200

    🔧 Fix broken command duplication (crash on more than one invokation of command)

--- src/Bot.ts
+++ src/Bot.ts
@@ -22,7 +22,7 @@ const logError = (message: string) => {
     console.error(`[${moment().format("DD-MM-YYYY HH:mm:ss")}]: ❌ ${message}`)
 };
 
-let commands = [];
+let commands: any[] = [];
 const loadCommands = async (bot: Eris.Client) => {
     log(`⏳ Loading commands...`);
     await glob("dist/commands/**/**/*.js")
@@ -40,31 +40,32 @@ const loadCommands = async (bot: Eris.Client) => {
                         type: Constants.ApplicationCommandTypes.CHAT_INPUT,
                     });
 
-                    bot.on("interactionCreate", async (interaction) => {
-                        if (interaction instanceof CommandInteraction) {
-                            if (!commands.some(command => command.name === interaction.data.name)) {
-                                return interaction.createMessage("❌ This command does not exist");
-                            }
-
-                            try {
-                                const command = commands.find(command => command.name === interaction.data.name)
-                                if (command) {
-                                    await command.execute(interaction);
-                                }
-                            } catch (exception) {
-                                logError(`Error executing command: ${exception}`);
-                                interaction.createMessage({
-                                    content: "❌ An error occured while executing command",
-                                });
-                            }
-                        }
-                    });
                 } catch (exception: any) {
                     logError(`Failed to load commands: ${exception}`)
                 }
             }
         });
 };
+
+bot.on("interactionCreate", async (interaction) => {
+    if (interaction instanceof CommandInteraction) {
+        if (!commands.some(command => command.name === interaction.data.name)) {
+            return interaction.createMessage("❌ This command does not exist");
+        }
+
+        try {
+            const command = commands.find(command => command.name === interaction.data.name);
+            if (command) {
+                await command.execute(interaction);
+            }
+        } catch (exception) {
+            logError(`Error executing command: ${exception}`);
+            interaction.createMessage({
+                content: "❌ An error occured while executing command",
+            });
+        }
+    };
+});
 
 bot.on("ready", async () => {
     loadCommands(bot);
