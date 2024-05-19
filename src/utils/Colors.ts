132385bc5c5e568e8d669ddae72b285d92074d22
eliza <sech1p@disroot.org>
2024-05-19 18:55:54 +0200

    ➕ [utils] Add Colors util

---
+++ src/utils/Colors.ts
@@ -0,0 +1,37 @@
+// Borrowed from hope project
+// https://github.com/sech1p/hope
+const Green: number = 0x00FF00;
+const Red: number = 0xFF0000;
+const Purple: number = 0x8E44AD;
+const Blue: number = 0x3498DB;
+const Yellow: number = 0x3498DB;
+const White: number = 0x3498DB;
+const Silver: number = 0x99A3A4;
+const Gray: number = 0x616A6B;
+const Pink: number = 0xFFC0CB;
+
+let RANDOM;
+RANDOM = [
+    Green,
+    Red,
+    Purple,
+    Blue,
+    Yellow,
+    White,
+    Silver,
+    Gray,
+    Pink,
+];
+
+export default {
+    Green,
+    Red,
+    Purple,
+    Blue,
+    Yellow,
+    White,
+    Silver,
+    Gray,
+    Pink,
+    RANDOM,
+};
